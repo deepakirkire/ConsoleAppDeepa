30886e996824f78cf81f25de3ea80db9f437984a
deepa <deepa@DESKTOP-OFR07K4>
2022-10-17 19:50:16 +0530

    Add project files.

---
+++ ConsoleAppDeepa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDeepa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
