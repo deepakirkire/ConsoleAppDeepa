4c462fcafb6608956783e178fc81299e25dbf49e
Deepa Kirkire <deepakirkire@outlook.com>
2022-10-17 20:16:34 +0530

    Added Method1() in Base customer class.

--- ConsoleAppDeepa/Program.cs
+++ ConsoleAppDeepa/Program.cs
@@ -5,6 +5,11 @@ namespace ConsoleAppDeepa
     class BaseCustomer
     {
         public string CustomerName { get; set; }
+        public void Method1()
+        {
+
+            Console.WriteLine("BaseCustomer Method1");
+        }
     }
     class Program
     {
